e4de6313969d0941396540b218ffec6f1cc731ee
Sergebryan <sergebryan1@gmail.com>
2020-09-19 22:04:36 -0500

    completed homework

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser")
 
 var app = express()
 
-var PORT = process.env.PORT || 8080
+var PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
